e3c7266cfe7fe1b1cf80741eb96829c46dc5ee85
光弘 <guanghong.wsj@alibaba-inc.com>
2017-06-06 15:35:54 +0800

    eslint

---
+++ demo/index.jsx
@@ -0,0 +1,14 @@
+/**
+ * Menu Component Demo for uxcore
+ * @author vincent.bian
+ *
+ * Copyright 2014-2015, Uxcore Team, Alinw.
+ * All rights reserved.
+ */
+
+ const React = require('react');
+ const ReactDOM = require('react-dom');
+
+ const Demo = require('./MenuDemo');
+
+ ReactDOM.render(<Demo />, document.getElementById('UXCoreDemo'));
