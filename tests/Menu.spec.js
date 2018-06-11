a93f058571f5963125aa983cc6b3702d57a9635d
micate <micate@me.com>
2018-06-11 10:47:47 +0800

    test: update test environment

--- tests/Menu.spec.js
+++ tests/Menu.spec.js
@@ -1,8 +1,11 @@
 import expect from 'expect.js';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils, { Simulate } from 'react-dom/test-utils';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 import Menu from '../src';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('Menu', () => {
 
